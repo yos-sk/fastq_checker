eb863823777219e7bb36eed3fd651e7ad813cebc
yos-sk <stl1frs2424@gmail.com>
2024-01-09 10:28:22 +0900

    Add extract.rs

---
+++ src/extract.rs
@@ -0,0 +1,56 @@
+use std::error::Error;
+use std::io::BufRead;
+
+use fastq_checker::open_file;
+
+pub fn run(input_file: &str, format: &str, length: usize) -> Result<(), Box<dyn Error>> {
+    let reader = open_file(input_file).expect(&format!("Could not open {}", input_file));
+
+    let mut read_id = String::new();
+    let mut sequence = String::new();
+    let mut quality = String::new();
+
+
+    if format == "fastq" {
+        for (i, line) in reader.lines().enumerate() {
+            let line = line?;
+            match i % 4 {
+                0 => read_id = line,
+                1 => sequence = line,
+                3 => quality = line,
+                _ => (),
+            }
+            if i % 4 == 3 {
+                if sequence.len() > length {
+                    println!("{}", read_id);
+                    println!("{}", sequence);
+                    println!("+");
+                    println!("{}", quality);
+                }
+            }
+        }
+    } else if format == "fasta" {
+        for line in reader.lines() {
+            let line = line?;
+            if line.starts_with(">") {
+                if !sequence.is_empty() {
+                    if sequence.len() > length {
+                        println!("{}", read_id);
+                        println!("{}", sequence);
+                    }
+                }
+                read_id = line;
+            } else {
+                sequence.push_str(&line);
+            }
+        }
+
+        if !sequence.is_empty() {
+            if sequence.len() > length {
+                println!("{}", read_id);
+                println!("{}", sequence);
+            }
+        }
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::process;
 
 mod check;
 mod rmdup;
+mod extract;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -28,6 +29,16 @@ enum Commands {
         #[arg(short = 'f', long)]
         format: String,
     },
+    Extract {
+        #[arg(short = 'i', long)]
+        input_file: String,
+
+        #[arg(short = 'f', long)]
+        format: String,
+
+        #[arg(short = 'l', long)]
+        length: usize,
+    },
 }
 
 fn main() {
@@ -45,6 +56,15 @@ fn main() {
         Commands::Rmdup{input_file,
                         format} => {
             if let Err(error) = rmdup::run(input_file, format) {
+                eprintln!("{}", error);
+                process::exit(1);
+            }
+        },
+
+        Commands::Extract{input_file,
+                        format,
+                        length} => {
+            if let Err(error) = extract::run(input_file, format, *length) {
                 eprintln!("{}", error);
                 process::exit(1);
             }
